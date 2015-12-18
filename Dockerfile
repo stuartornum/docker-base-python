864d2a110a885d33b02bf18e6bf31f03f6cb2651
Stuart Munro <stuart@munro.co>
2015-12-18 16:39:40 +0000

    Upgraded to Python 2.7.11, pip 7.1.2 and setuptools 19.1.1

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,16 @@
-FROM phusion/baseimage:0.9.16
+FROM phusion/baseimage:0.9.18
 
 MAINTAINER Stuart Munro <stuart@munro.co>
 
 # Dependencies
 RUN DEBIAN_FRONTEND='noninteractive' \
   apt-get update && \
-  apt-get -y --force-yes install bash apt-utils build-essential git software-properties-common libpq-dev g++ make libpcre3 libpcre3-dev \
-  libxslt-dev libxml2-dev wget libmysqlclient-dev
+  apt-get -y --force-yes install bash apt-utils build-essential git software-properties-common \
+  libpq-dev g++ make libpcre3 libpcre3-dev libxslt-dev libxml2-dev wget libmysqlclient-dev
 
 RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-# Install latest python 2.7.9
+# Install latest python 2.7.11
 ADD ./docker/install_python.sh /install_python.sh
 RUN chmod 755 /install_python.sh
 RUN /install_python.sh
--- docker/install_python.sh
+++ docker/install_python.sh
@@ -2,24 +2,24 @@
 
 set -e
 
-# INSTALL PYTHON 2.7.9
+# INSTALL PYTHON 2.7.11
 cd /tmp
-wget https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz
-tar zxvf Python-2.7.9.tgz
-cd Python-2.7.9
+wget https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz
+tar zxvf Python-2.7.11.tgz
+cd Python-2.7.11
 ./configure && make && make install
 cd /tmp && rm -rf Python-*
 
 # INSTALL PYTHON SETUPTOOLS
-wget https://pypi.python.org/packages/source/s/setuptools/setuptools-15.2.tar.gz#md5=a9028a9794fc7ae02320d32e2d7e12ee
-tar zxvf setuptools-15.2.tar.gz
-cd setuptools-15.2
+wget https://pypi.python.org/packages/source/s/setuptools/setuptools-19.1.1.tar.gz#md5=792297b8918afa9faf826cb5ec4a447a
+tar zxvf setuptools-19.1.1.tar.gz
+cd setuptools-19.1.1
 /usr/local/bin/python2.7 setup.py install
 cd /tmp && rm -rf setuptools*
 
 # INSTALL PYTHON PIP
-wget https://pypi.python.org/packages/source/p/pip/pip-6.1.1.tar.gz#md5=6b19e0a934d982a5a4b798e957cb6d45
-tar zxvf pip-6.1.1.tar.gz
-cd pip-6.1.1
+wget https://pypi.python.org/packages/source/p/pip/pip-7.1.2.tar.gz#md5=3823d2343d9f3aaab21cf9c917710196
+tar zxvf pip-7.1.2.tar.gz
+cd pip-7.1.2
 /usr/local/bin/python2.7 setup.py install
 
